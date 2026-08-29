83b53dcde7212aa4647d7f2bb691a49f4461edd8
Benjamin Contreras <becontreras@alumnos.uai.cl>
2026-08-29 16:40:51 -0400

    fix: align renamed frontend imports

--- frontend/src/pages/Bienvenida.jsx
+++ frontend/src/pages/Bienvenida.jsx
@@ -1,12 +1,12 @@
-import './bienvenida.css';
+import './Bienvenida.css';
 import LoginButton from '../components/Login/LoginButton';
 
 function Bienvenida() {
 	return (
-		<div className='Page-0'>
-			<div className='Bienvenida'>
-				<h1 className='Bienvenida-titulo'> Bienvenido a EdiRegs</h1>
-                <LoginButton />
+		<div className="Page-0">
+			<div className="Bienvenida">
+				<h1 className="Bienvenida-titulo">Bienvenido a EdiReg</h1>
+				<LoginButton />
 			</div>
 		</div>
 	);
